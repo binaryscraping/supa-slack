c7f1f98581dc33e6b2bd3998182ee2ca6bbab7b6
Guilherme Souza <grsouza@pm.me>
2022-12-23 14:43:11 -0300

    Project init

---
+++ SupaSlack/SupaSlackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaSlackApp.swift
+//  SupaSlack
+//
+//  Created by Guilherme Souza on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SupaSlackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
