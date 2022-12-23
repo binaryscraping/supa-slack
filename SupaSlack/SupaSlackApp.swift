56bd58d12f0bb1c53408c55066a9bed73af715b9
Guilherme Souza <grsouza@pm.me>
2022-12-23 14:46:49 -0300

    Add dependencies

--- SupaSlack/SupaSlackApp.swift
+++ SupaSlack/SupaSlackApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SupaSlackApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
